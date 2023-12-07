fcb4fb24203a3406220e9878728c653d67488c66
shahrukh khan <shahrukh.khan072@gmail.com>
2023-12-07 20:50:23 +0530

    setup of blog backend

---
+++ server.js
@@ -0,0 +1,161 @@
+import express, { json } from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+import bcrypt from "bcrypt";
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import cors from "cors"
+import admin  from "firebase-admin";
+import serviceAccountKey from "./full-stack-blog-website-firebase-adminsdk-r30um-c029f6e770.json" assert{type:"json"}
+import { getAuth } from "firebase-admin/auth";
+
+//schema
+import User from "./Schema/User.js";
+
+const server = express();
+let PORT = 4040;
+
+admin.initializeApp({
+   credential: admin.credential.cert(serviceAccountKey)
+})
+
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+server.use(express.json());
+server.use(cors());
+
+mongoose.connect(process.env.DB_LOCATION, {
+    autoIndex: true
+});
+
+const formateDatatoSend = (user) =>{
+
+    const access_token = jwt.sign({id: user._id}, process.env.SECRET_ACCESS_KEY)
+    return{
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname
+    }
+}
+const generateUsername = async(email) =>{
+    let username = email.split("@")[0];  
+
+    let isUsernameNotUnique = await User.exists({"personal_info.username": username}).then((result) => result)
+    isUsernameNotUnique ? username += nanoid() : "";
+    return username;
+}
+server.post("/signup", (req,res) => {
+    let {fullname, email, password} = req.body;
+    if(fullname.length < 3){
+        return res.status(403).json({"error": "Fullname must be at leat 3 letter"})
+    }
+    if(!email.length){
+        return res.status(403).json({"error": "Enter Email"})
+    }
+    if(!emailRegex.test(email)){
+        return res.status(403).json({"error": "Email is invalid"})
+    }
+    if(!passwordRegex.test(password)){
+        return res.status(403).json({"error": "Password should be 6 to 20 character long with 1 numeric, 1 lowercase, 1 uppercase"})
+    }
+    bcrypt.hash(password, 10, async(err, hashed_password) =>{
+        let username = await generateUsername(email); 
+        let user = new User({
+            personal_info:{
+                fullname,
+                email,
+                password: hashed_password,
+                username
+            }
+        })
+        user.save().then((u) => {
+            return res.status(200).json(formateDatatoSend(u))
+        })
+        .catch(err =>{
+            if(err.code == 11000){
+                return res.status(500).json({"error": "Emaail already exist"})
+            }
+            return res.status(500).json({"error": err.message})
+        })
+    })
+})
+
+server.post("/signin", (req, res) =>{
+    let {email, password} = req.body;
+
+    User.findOne({"personal_info.email": email})
+    .then((user) =>{
+        if(!user){
+            return res.status(403).json({"error":"Email not found"});
+        }
+        if(!user.google_auth){
+            bcrypt.compare(password, user.personal_info.password, (err, result) => {
+                if(err){
+                    return res.status(403).json({"error": "Error occured while login please try again"});
+                }
+    
+                if(!result){
+                    return res.status(403).json({"error": "Incorrect password"});
+                }else{
+                    return res.status(200).json(formateDatatoSend(user))
+                }
+            })            
+        }else{
+            return res.status(403).json({"error": "Account was created using google. try logging with google"})
+        }      
+    })
+    .catch(err =>{
+        console.log(err.message);
+        return res.status(500).json({"error": err.message});
+    })
+})
+
+server.post("/google-auth", async(req, res) =>{
+   
+    let { access_token } = req.body;
+    
+    getAuth()
+    .verifyIdToken(access_token)
+    .then(async(decodedUser) =>{
+         
+        let {email, name, picture } = decodedUser;
+
+        picture = picture.replace("s96-c", "s384-c");
+
+        let user = await User.findOne({"personal_info.email": email}).select("personal_info.fullname personal_info.profile_img personal_info.username google_auth").then((u) =>{
+            return u || null
+        })
+        .catch(err =>{
+            return res.status(500).json({"error": err.message})
+        })
+
+        if(user){
+           if(!user.google_auth){
+            return res.status(403).json({"error": "This email was singed up without google, please log in with password to access the account" })
+           }
+        }
+        else{
+            let username = await generateUsername(email)
+            user = new User({
+                personal_info: {fullname: name, email, profile_img: picture, username, google_auth: true }
+            })
+
+            await user.save().then((u) =>{
+                user = u;
+            })
+            .catch(err =>{
+                return res.status(500).json({"error": err.message})
+            })
+        }
+        return res.status(200).json(formateDatatoSend(user))
+    })
+    .catch(err =>{
+        return res.status(500).json({"error": "Failed to authanticate you with google. try with some other google account "})
+    })
+})
+
+server.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
