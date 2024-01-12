42d846748e8ad926ae5fd6cc5b7f38106a135010
shahrukh khan <shahrukh.khan072@gmail.com>
2024-01-12 22:11:15 +0530

    add comment and like

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ import aws from "aws-sdk";
 //schema
 import User from "./Schema/User.js";
 import Blog from "./Schema/Blog.js";
+import Notification from "./Schema/Notification.js"
+import Comment from "./Schema/Comment.js"
 
 const server = express();
 let PORT = 4040;
@@ -421,6 +423,124 @@ server.post("/get-blog", (req, res) =>{
     })
     .catch(err =>{
         return res.status(500).json({error: err.message});
+    })
+})
+
+server.post("/like-blog", verifyJWT, (req, res) =>{
+
+    let user_id = req.user;
+
+    let {_id, islikedByUsers} = req.body;
+
+    let incrementval = !islikedByUsers ? 1 : -1;
+
+    Blog.findOneAndUpdate({_id}, {$inc: {"activity.total_likes": incrementval}})
+    .then(blog =>{
+        if(!islikedByUsers){
+            let like = new Notification({
+                type: "like",
+                blog: _id,
+                notification_for: blog.author,
+                user: user_id       
+            })
+
+            like.save().then(notification =>{
+                return res.status(200).json({liked_by_user: true})
+            })
+
+        } else{
+            Notification.findOneAndUpdate({user: user_id, blog: _id, type: "like"})
+            .then(data =>{
+                return res.status(200).json({liked_by_user: false})
+            })
+            .catch(err =>{
+                return res.status(500).json({error: err.message})
+            })
+        }
+    })
+})
+
+server.post("/isliked-by-user", verifyJWT, (req, res) =>{
+    
+    let user_id = req.user;
+    let {  _id }= req.body;
+
+    Notification.exists({user : user_id, type: "like", blog: _id})
+    .then(result =>{
+        return res.status(200).json({result})
+    })
+    .catch(err =>{
+       return res.status(500).json({error: err.message}) 
+    })
+})
+
+server.post("/add-comment", verifyJWT, (req, res) =>{
+    let user_id = req.user;
+
+    let { _id, comment, blog_author, replying_to } = req.body;
+
+    if(!comment.length){
+        return res.status(403).json({error: "write something to leave a comment."})
+    }
+
+    let commentObj = {
+        blog_id: _id,
+        blog_author,
+        comment,
+        commented_by:user_id,
+    }
+    if(replying_to){
+        commentObj.parent = replying_to;
+    }
+
+   new Comment (commentObj).save().then(async commentFile => {
+
+        let {comment, commentedAt, children} = commentFile;
+
+        Blog.findOneAndUpdate({_id}, {$push: { "comments": commentFile._id}, $inc: {"activity.total_comments": 1, "activity.total_parent_comments": replying_to ? 0 : 1}, })
+    .then(blog =>{console.log("New Comment created")});
+
+    let notificationObj ={
+        type: replying_to ? "reply" : "comment",
+        blog: _id,
+        notification_for: blog_author,
+        user: user_id,
+        comment: commentFile._id
+    }
+
+    if(replying_to){
+        notificationObj.replied_on_comment = replying_to;
+
+        await Comment.findOneAndUpdate({ _id: replying_to}, {$push: {children: commentFile._id}})
+        .then(replyingToCommentDoc => {notificationObj.notification_for = replyingToCommentDoc.commented_by})
+    }
+
+    new Notification(notificationObj).save().then(notificationObj => console.log("new notification created"));
+
+    return res.status(200).json({comment, commentedAt, _id: commentFile._id, user_id: children})
+
+    })
+})
+
+server.post("/get-blog-comments", (req, res) =>{
+
+    let {blog_id, skip} = req.body;
+
+    let maxLimit = 5;
+
+    Comment.find({blog_id, isReply: false})
+    .populate("commented_by", "personal_info.username personal_info.fullname personal_info.profile_img")
+    .skip(skip)
+    .limit(maxLimit)
+    .sort({
+        'commentedAt': -1
+    })
+    .then(comment =>{
+        return res.status(200).json(comment);
+    })
+    .catch(err =>{
+        console.log(err.message);
+        return res.status(500).json({error: err.message})
     })
 })
 
